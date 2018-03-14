ac7bd2ed688a09268aa75cc61ba4d6fa0c122fed
rufo <rufus@rufus.al>
2018-03-14 19:52:07 +0000

    bump deps

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
 	:license
 		{:name "Eclipse Public License"
 		 :url "http://www.eclipse.org/legal/epl-v10.html"}
-	:dependencies [[org.clojure/clojure "1.7.0"]]
+	:dependencies [[org.clojure/clojure "1.9.0"]]
 	:profiles
 		{:dev
 			{:dependencies
-				[[midje "1.7.0"]
+				[[midje "1.9.1"]
 				 [slingshot "0.12.2"]]
 			 :plugins
 				[[lein-midje "3.2"]]}}
